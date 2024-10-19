26ac26d27c425125a80961814a4b24ffdd0f40f6
Jannik Baranczyk <8103530+jannbar@users.noreply.github.com>
2024-10-19 18:03:45 +0200

    wip

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from "astro/config"
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jatnnik.dev",
   integrations: [tailwind()],
   markdown: {
     shikiConfig: {
       theme: "catppuccin-frappe",
     },
   },
-});
+  output: "static",
+})
---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const SITE_INFO = {
+  TITLE: "Jannik Baranczyk",
+  DESCRIPTION:
+    "Full-Stack Developer with 10+ years experience. My tools of choice are React, TypeScript, Laravel and Tailwind CSS.",
+}
