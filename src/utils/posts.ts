4a410c338c182f1484e7a882ac67e94d82a1b096
Jannik Baranczyk <8103530+jatnnik@users.noreply.github.com>
2025-08-27 10:29:42 +0200

    improvements

--- src/utils/posts.ts
+++ src/utils/posts.ts
@@ -1,12 +1,22 @@
 import { getCollection, type CollectionEntry } from 'astro:content'
 
+interface GetSortedPostsOpts {
+  limit?: number
+}
+
 /**
  * Get all posts, sorted by publication date
  */
-export async function getSortedPosts() {
+export async function getSortedPosts(opts: GetSortedPostsOpts = {}) {
   const posts = await getCollection('posts')
-  return posts.sort(
+  const sortedPosts = posts.sort(
     (a: CollectionEntry<'posts'>, b: CollectionEntry<'posts'>) =>
       b.data.pubDate.valueOf() - a.data.pubDate.valueOf(),
   )
+
+  if (opts.limit) {
+    return sortedPosts.slice(0, opts.limit)
+  }
+
+  return sortedPosts
 }
